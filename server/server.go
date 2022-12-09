946c380d0d8a9c96c6c8b477aef8964defc80c41
Kwitsch <kwitsch@gmail.com>
2022-12-09 23:47:48 +0100

    started server

--- server/server.go
+++ server/server.go
@@ -1,1 +1,37 @@
 package server
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/kwitsch/GoSimpleServe/files"
+	"github.com/kwitsch/GoSimpleServe/util"
+)
+
+const serverPort = 80
+
+type Server struct {
+	log util.Log
+	mux *http.ServeMux
+}
+
+func New(verbose bool) *Server {
+	s := Server{
+		log: *util.NewLog("server", verbose),
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(files.StaticFilesDir)))
+	mux.HandleFunc("/files", s.getFiles)
+
+	s.mux = mux
+
+	return &s
+}
+
+func (s *Server) getFiles(w http.ResponseWriter, r *http.Request) {
+	s.log.V("got /files request")
+	f := files.GetFiles()
+	s.log.V("responese:\n", f)
+	io.WriteString(w, f)
+}
